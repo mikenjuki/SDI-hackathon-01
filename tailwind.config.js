249dbeb01929a317691979e84799159ef762eb99
Mike Njuki <mikenjuki@gmail.com>
2024-10-26 14:45:35 +0200

    linked tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{html,js}",
+    "./components/**/*.{html,js}",
+    "./home.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
